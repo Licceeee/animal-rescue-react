31cada12fc1064c175d4505c441ffaf4eda4c6c7
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-08-25 22:36:24 +0200

    add chakra ui provider

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import "./index.css";
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ApolloProvider } from '@apollo/react-hooks';
-import { client } from './api/client';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ChakraProvider } from "@chakra-ui/react";
+import { client } from "./api/client";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <Router>
     <React.StrictMode>
       <ApolloProvider client={client}>
-        <App />
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
       </ApolloProvider>
     </React.StrictMode>
   </Router>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
