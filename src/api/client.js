7fa7f8a9e6f5b31a5f4fcb0e856cc6f4798e3e03
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-08-26 08:35:20 +0200

    add url vars - server, local

--- src/api/client.js
+++ src/api/client.js
@@ -1,12 +1,16 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+// eslint-disable-next-line no-unused-vars
+const server = "https://paw-api.tornode.org/graphql/";
+// eslint-disable-next-line no-unused-vars
+const local = "http://127.0.0.1:8000/graphql/";
+
 const client = new ApolloClient({
-  uri: "http://127.0.0.1:8000/graphql/",
+  uri: server,
   cache: new InMemoryCache(),
   fetchOptions: {
     mode: "no-cors",
   },
 });
-
 
 export { client };
