960078947b58e2cefbb6aad065c8e601a32a0c28
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-08-26 08:35:45 +0200

    add register api mutation

--- src/api/auth.js
+++ src/api/auth.js
@@ -9,4 +9,19 @@ const VERIFYACCOUNT = gql`
   }
 `;
 
-export { VERIFYACCOUNT };
+const REGISTER = gql`
+  mutation Register($email: String!, $password1: String!, $password2: String!) {
+    register(email: $email, password1: $password1, password2: $password2) {
+      token
+      refreshToken
+      success
+      errors
+      unarchiving
+      user {
+        email
+      }
+    }
+  }
+`;
+
+export { VERIFYACCOUNT, REGISTER };
