9d903de6fa9f31ed42975734ac13b15559e79d78
Alicia Schonefeld <schonefeld.dev@gmail.com>
2023-05-25 14:13:41 +0200

    mix changes

---
+++ Snippets/snippets.js
@@ -0,0 +1,94 @@
+
+/* <form onSubmit={handleSubmit(onSubmit)}>
+<FormControl isInvalid={errors.name}>
+  <FormLabel htmlFor="email">Email</FormLabel>
+  <Input
+    id="email"
+    placeholder="Your Email here"
+    {...register("email", {
+      required: "This is required",
+      minLength: { value: 4, message: "Minimum length should be 4" },
+    })}
+  />
+  <FormErrorMessage>
+    {errors.name && errors.name.message}
+  </FormErrorMessage>
+</FormControl>
+<FormControl isInvalid={errors.name}>
+  <FormLabel htmlFor="password1">Password</FormLabel>
+  <Input
+    id="password1"
+    placeholder="Password"
+    {...register("password1", {
+      required: "This is required",
+      minLength: { value: 4, message: "Minimum length should be 4" },
+    })}
+  />
+  <FormErrorMessage>
+    {errors.name && errors.name.message}
+  </FormErrorMessage>
+</FormControl>
+<Button
+  mt={4}
+  colorScheme="teal"
+  isLoading={isSubmitting}
+  type="submit"
+>
+  Submit
+</Button>
+</form> */
+
+
+{/* <FormControl isInvalid={errors.name}>
+<FormLabel htmlFor="email">First name</FormLabel>
+
+<Input
+  id="email"
+  type="email"
+  placeholder="Email"
+  {...register("Email", {
+    required: "This is required",
+    pattern: /^\S+@\S+$/i,
+  })}
+/>
+<FormErrorMessage>
+  {errors.name && errors.name.message}
+</FormErrorMessage>
+{/* <Input
+  type="password"
+  id="password1"
+  placeholder="Password"
+  {...register("Password", {
+    required: true,
+    min: 8,
+    maxLength: 100,
+  })}
+/>
+<Input
+  type="password"
+  id="password2"
+  placeholder="Password Repeat"
+  {...register("Password Repeat", {
+    required: true,
+    min: 8,
+    maxLength: 80,
+  })}
+/> */
+// </FormControl>
+// <Button
+// mt={4}
+// colorScheme="teal"
+// isLoading={isSubmitting}
+// type="submit"
+// >
+// Submit
+// </Button> */
+
+
+<>
+
+</>
+<>
+
+
+</>
